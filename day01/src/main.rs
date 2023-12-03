ae5146030a1467a9f5c2d2bd37fdbcca9e3e8d77
harryjulian <harryjulian16@gmail.com>
2023-12-03 16:28:45 +0000

    day 1 part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -16,30 +16,50 @@ fn _find_digits(s: &str) -> Vec<i32> {
     result
 }
 
-fn _find_digits_or_words(s: &str) {}
+fn _find_digits_and_words(s: &str, words: &Vec<&str>) -> Vec<i32> {
+    let mut digits: Vec<i32> = vec![];
+    for idx in 0..s.len() {
+        let v = s.chars().nth(idx).unwrap();
+        if v.is_numeric() {
+            digits.push(v.to_digit(10).unwrap() as i32);
+            continue;
+        }
+        for (k, v) in words.iter().enumerate() {
+            if s[idx..].starts_with(v) {
+                digits.push(k as i32 + 1);
+                continue;
+            }
+        }
+    }
+    vec![*digits.first().unwrap(), *digits.last().unwrap()]
+}
 
 fn solve_part_one() -> i32 {
-    let input: Vec<Vec<i32>> = include_str!("input.txt")
+    include_str!("../data/input.txt")
         .split("\n")
         .into_iter()
-        .map(|s| _find_digits(s))
-        .collect();
-    input.iter().map(|x| (x[0] * 10) + x[1]).sum()
+        .map(|s| {
+            let digits = _find_digits(s);
+            (digits[0] * 10) + digits[1]
+        })
+        .sum()
 }
 
 fn solve_part_two() -> i32 {
-    let input: Vec<Vec<i32>> = include_str!("input.txt")
+    let words: Vec<&str> = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    include_str!("../data/input.txt")
         .split("\n")
         .into_iter()
-        .map(|s| _find_digits_or_words(s))
-        .collect();
-    input.iter().map(|x| (x[0] * 10) + x[1]).sum()
+        .map(|s| {
+            let digits = _find_digits_and_words(s, &words);
+            (digits[0] * 10) + digits[1]
+        })
+        .sum()
 }
 
 fn main() {
-    let part: i32 = str::parse::<i32>(&std::env::args().nth(1).expect("no pattern given")).unwrap();
-    match part {
-        1 => println!("The answer to part 1 is: {:?}", solve_part_one()),
-        2 => println!("The answer to part 2 is: {:?}", solve_part_two()),
-    }
+    println!("The answer to part 1 is: {:?}", solve_part_one());
+    println!("The answer to part 2 is: {:?}", solve_part_two());
 }
